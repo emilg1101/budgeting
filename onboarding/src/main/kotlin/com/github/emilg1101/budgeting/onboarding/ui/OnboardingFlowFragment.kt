82a1913e2540949155fafa9a0f7287821680dcb3
emilg1101 <emilyandro99@gmail.com>
2020-09-02 21:21:20 +0300

    add onboarding screens

--- onboarding/src/main/kotlin/com/github/emilg1101/budgeting/onboarding/ui/OnboardingFlowFragment.kt
+++ onboarding/src/main/kotlin/com/github/emilg1101/budgeting/onboarding/ui/OnboardingFlowFragment.kt
@@ -1,6 +1,8 @@
 package com.github.emilg1101.budgeting.onboarding.ui
 
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.emilg1101.budgeting.core.base.NestedFragment
@@ -11,7 +13,8 @@ import com.github.emilg1101.budgeting.onboarding.di.DaggerOnboardingComponent
 import com.github.emilg1101.budgeting.onboarding.di.OnboardingComponent
 import javax.inject.Inject
 
-class OnboardingFlowFragment : NestedFragment<OnboardingFlowViewModel>(R.layout.fragment_onboarding_flow) {
+class OnboardingFlowFragment :
+    NestedFragment<OnboardingFlowViewModel>(R.layout.fragment_onboarding_flow) {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -24,6 +27,14 @@ class OnboardingFlowFragment : NestedFragment<OnboardingFlowViewModel>(R.layout.
     override fun onCreate(savedInstanceState: Bundle?) {
         DaggerOnboardingComponent.builder().coreComponent(coreComponent()).build().inject(this)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setOnApplyWindowInsetsListener { v, insets ->
+            v.updatePadding(top = insets.systemWindowInsetTop)
+            insets
+        }
     }
 }
 
